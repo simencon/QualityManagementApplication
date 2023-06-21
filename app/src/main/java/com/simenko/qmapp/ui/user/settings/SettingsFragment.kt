f9e98d0631d0676ef659018518e85cbb16909224
VSemenyshyn <Viktoriia.Palenka@gmail.com>
2023-06-21 15:11:22 +0300

    Compose - now settings is fragment.

--- app/src/main/java/com/simenko/qmapp/ui/user/settings/SettingsFragment.kt
+++ app/src/main/java/com/simenko/qmapp/ui/user/settings/SettingsFragment.kt
@@ -2,9 +2,9 @@ package com.simenko.qmapp.ui.user.settings
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.activity.compose.setContent
-import androidx.activity.viewModels
-import androidx.appcompat.app.AppCompatActivity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -13,10 +13,13 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import com.simenko.qmapp.ui.user.login.LoginActivity
 import com.simenko.qmapp.ui.user.user.UserManager
 import com.simenko.qmapp.ui.theme.QMAppTheme
@@ -24,34 +27,38 @@ import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
 @AndroidEntryPoint
-class SettingsActivity : AppCompatActivity() {
+class SettingsFragment : Fragment() {
 
-    private val settingsViewModel: SettingsViewModel by viewModels()
+    private val settingsViewModel: SettingsViewModel by activityViewModels()
 
     @Inject
     lateinit var userManager: UserManager
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        setContent {
-            QMAppTheme {
-                Settings(
-                    modifier = Modifier
-                        .padding(all = 0.dp)
-                        .fillMaxWidth(),
-                    onCLickRefreshNotifications = {
-                        settingsViewModel.refreshNotifications()
-                    },
-                    onClickLogOut = {
-                        settingsViewModel.logout()
-                        val intent = Intent(this, LoginActivity::class.java)
-                        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or
-                                Intent.FLAG_ACTIVITY_CLEAR_TASK or
-                                Intent.FLAG_ACTIVITY_NEW_TASK
-                        startActivity(intent)
-                    }
-                )
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return ComposeView(requireContext()).apply {
+            setContent {
+                QMAppTheme {
+                    Settings(
+                        modifier = Modifier
+                            .padding(all = 0.dp)
+                            .fillMaxWidth(),
+                        onCLickRefreshNotifications = {
+                            settingsViewModel.refreshNotifications()
+                        },
+                        onClickLogOut = {
+                            settingsViewModel.logout()
+                            val intent = Intent(activity, LoginActivity::class.java)
+                            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or
+                                    Intent.FLAG_ACTIVITY_CLEAR_TASK or
+                                    Intent.FLAG_ACTIVITY_NEW_TASK
+                            startActivity(intent)
+                        }
+                    )
+                }
             }
         }
     }
