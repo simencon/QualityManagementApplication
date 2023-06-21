b6ba928cfa3b64488bbf53c904a85f283a588128
VSemenyshyn <Viktoriia.Palenka@gmail.com>
2023-06-21 12:36:13 +0300

    Compose - no more Fragments in RegistrationActivity - changed to Composable

--- app/src/main/java/com/simenko/qmapp/ui/auth/registration/RegistrationActivity.kt
+++ app/src/main/java/com/simenko/qmapp/ui/auth/registration/RegistrationActivity.kt
@@ -3,42 +3,21 @@ package com.simenko.qmapp.ui.auth.registration
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.activity.viewModels
-import com.simenko.qmapp.R
-import com.simenko.qmapp.databinding.ActivityRegistrationBinding
-import com.simenko.qmapp.ui.auth.registration.enterdetails.EnterDetailsFragment
-import com.simenko.qmapp.ui.auth.registration.termsandconditions.TermsAndConditionsFragment
+import androidx.activity.compose.setContent
+import com.simenko.qmapp.ui.auth.Navigation
 import com.simenko.qmapp.ui.main.MainActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class RegistrationActivity : AppCompatActivity() {
-    private val registrationViewModel: RegistrationViewModel by viewModels()
-
-    private lateinit var binding: ActivityRegistrationBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityRegistrationBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.fragment_holder, EnterDetailsFragment()).commit()
+        setContent { Navigation() }
     }
 
-    /**
-     * Callback from EnterDetailsFragment when username and password has been entered
-     */
-    fun onDetailsEntered() {
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.fragment_holder, TermsAndConditionsFragment())
-            .addToBackStack(TermsAndConditionsFragment::class.java.simpleName)
-            .commit()
-    }
 
     fun onTermsAndConditionsAccepted() {
-        registrationViewModel.registerUser()
         startActivity(Intent(this, MainActivity::class.java))
         finish()
     }
--- app/src/main/java/com/simenko/qmapp/ui/auth/registration/enterdetails/EnterDetails.kt
+++ app/src/main/java/com/simenko/qmapp/ui/auth/registration/enterdetails/EnterDetails.kt
@@ -1,11 +1,5 @@
 package com.simenko.qmapp.ui.auth.registration.enterdetails
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.EditText
-import android.widget.TextView
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -15,78 +9,49 @@ import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.ComposeView
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.activityViewModels
-import androidx.fragment.app.viewModels
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.Observer
-import androidx.lifecycle.viewmodel.compose.viewModel
-import com.simenko.qmapp.ui.auth.registration.RegistrationActivity
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.simenko.qmapp.ui.auth.Screen
 import com.simenko.qmapp.ui.auth.registration.RegistrationViewModel
 import com.simenko.qmapp.ui.theme.QMAppTheme
-import dagger.hilt.android.AndroidEntryPoint
-
-@AndroidEntryPoint
-class EnterDetailsFragment : Fragment() {
-
-    private val registrationViewModel: RegistrationViewModel by activityViewModels()
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        return ComposeView(requireContext()).apply {
-            setContent {
-                QMAppTheme {
-                    EnterDetails(
-                        modifier = Modifier
-                            .padding(all = 0.dp)
-                            .fillMaxWidth(),
-                        registrationViewModel = registrationViewModel,
-                        onSuccess = {
-                            (activity as RegistrationActivity).onDetailsEntered()
-                        }
-                    )
-                }
-            }
-        }
-    }
-}
 
 @Composable
 fun EnterDetails(
     modifier: Modifier,
-    registrationViewModel: RegistrationViewModel = hiltViewModel(),
-    enterDetailsViewModel: EnterDetailsViewModel = hiltViewModel(),
-    onSuccess: () -> Unit = {}
+    navController: NavHostController = rememberNavController(),
 ) {
-    val state by enterDetailsViewModel.enterDetailsState.observeAsState()
+    val registrationViewModel: RegistrationViewModel = hiltViewModel()
+    val enterDetailsViewModel: EnterDetailsViewModel = hiltViewModel()
+
+    val stateEvent by enterDetailsViewModel.enterDetailsState.collectAsStateWithLifecycle()
 
     var userNameText by rememberSaveable { mutableStateOf("") }
     var passwordText by rememberSaveable { mutableStateOf("") }
     var errorText by rememberSaveable { mutableStateOf("") }
 
-    when (state) {
-        is EnterDetailsSuccess -> {
-            registrationViewModel.updateUserData(userNameText, passwordText)
-            onSuccess()
+    stateEvent.getContentIfNotHandled()?.let { state ->
+        when (state) {
+            is EnterDetailsSuccess -> {
+                registrationViewModel.updateUserData(userNameText, passwordText)
+                navController.navigate(Screen.TermsAndConditions.withArgs(userNameText))
+            }
+
+            is EnterDetailsError -> {
+                errorText = state.error
+            }
+
+            is EnterDetailsInitialState -> {}
         }
-        is EnterDetailsError -> {
-            errorText = (state as EnterDetailsError).error
-        }
-        else -> {}
     }
 
     Column(
@@ -103,7 +68,7 @@ fun EnterDetails(
         )
         TextField(value = userNameText, onValueChange = { userNameText = it })
         TextField(value = passwordText, onValueChange = { passwordText = it })
-        if (state is EnterDetailsError)
+        if (errorText != "")
             Text(
                 text = errorText,
                 style = MaterialTheme.typography.labelSmall.copy(fontSize = 14.sp),
