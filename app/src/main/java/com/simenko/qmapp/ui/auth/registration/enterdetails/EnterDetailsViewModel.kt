892d0062981b6f1707b95d0856a04c1ced4beb57
VSemenyshyn <Viktoriia.Palenka@gmail.com>
2023-06-21 12:36:13 +0300

    Compose - no more Fragments in RegistrationActivity - changed to Composable

--- app/src/main/java/com/simenko/qmapp/ui/auth/registration/enterdetails/EnterDetailsViewModel.kt
+++ app/src/main/java/com/simenko/qmapp/ui/auth/registration/enterdetails/EnterDetailsViewModel.kt
@@ -1,9 +1,10 @@
 package com.simenko.qmapp.ui.auth.registration.enterdetails
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.simenko.qmapp.other.Event
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
 
 private const val MAX_LENGTH = 5
@@ -15,21 +16,23 @@ private const val MAX_LENGTH = 5
 @HiltViewModel
 class EnterDetailsViewModel @Inject constructor() : ViewModel() {
 
-    private val _enterDetailsState = MutableLiveData<EnterDetailsViewState>()
-    val enterDetailsState: LiveData<EnterDetailsViewState>
+    private val _enterDetailsState = MutableStateFlow<Event<EnterDetailsViewState>>(Event(EnterDetailsInitialState))
+    val enterDetailsState: StateFlow<Event<EnterDetailsViewState>>
         get() = _enterDetailsState
 
     fun validateInput(username: String, password: String) {
         when {
             username.length < MAX_LENGTH -> _enterDetailsState.value =
-                EnterDetailsError("Username has to be longer than 4 characters")
+                Event(EnterDetailsError("Username has to be longer than 4 characters"))
             password.length < MAX_LENGTH -> _enterDetailsState.value =
-                EnterDetailsError("Password has to be longer than 4 characters")
-            else -> _enterDetailsState.value = EnterDetailsSuccess
+                Event(EnterDetailsError("Password has to be longer than 4 characters"))
+            else -> _enterDetailsState.value = Event(EnterDetailsSuccess)
         }
     }
 }
 
 sealed class EnterDetailsViewState
+
+object EnterDetailsInitialState : EnterDetailsViewState()
 object EnterDetailsSuccess : EnterDetailsViewState()
 data class EnterDetailsError(val error: String) : EnterDetailsViewState()
--- app/src/main/java/com/simenko/qmapp/ui/auth/registration/termsandconditions/TermsAndConditions.kt
+++ app/src/main/java/com/simenko/qmapp/ui/auth/registration/termsandconditions/TermsAndConditions.kt
@@ -1,9 +1,5 @@
 package com.simenko.qmapp.ui.auth.registration.termsandconditions
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -12,54 +8,36 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.activityViewModels
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.simenko.qmapp.ui.auth.registration.RegistrationActivity
 import com.simenko.qmapp.ui.auth.registration.RegistrationViewModel
 import com.simenko.qmapp.ui.theme.QMAppTheme
-import dagger.hilt.android.AndroidEntryPoint
-
-@AndroidEntryPoint
-class TermsAndConditionsFragment : Fragment() {
-
-    private val registrationViewModel: RegistrationViewModel by activityViewModels()
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        return ComposeView(requireContext()).apply {
-            setContent {
-                QMAppTheme {
-                    TermsAndConditions(
-                        modifier = Modifier
-                            .padding(all = 0.dp)
-                            .fillMaxWidth(),
-                    ) {
-                        registrationViewModel.acceptTCs()
-                        (activity as RegistrationActivity).onTermsAndConditionsAccepted()
-                    }
-                }
-            }
-        }
-    }
-}
 
 @Composable
 fun TermsAndConditions(
     modifier: Modifier,
-    onClickRegister: () -> Unit,
+    registrationViewModel: RegistrationViewModel = hiltViewModel(),
+    user: String? = null
 ) {
+    val context = LocalContext.current
+
     Column(
         modifier = Modifier
             .padding(all = 0.dp)
     ) {
+        Text(
+            text = "Hello, $user",
+            style = MaterialTheme.typography.labelSmall.copy(fontSize = 18.sp),
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            modifier = Modifier
+                .padding(top = 0.dp, start = 0.dp, end = 0.dp, bottom = 0.dp)
+        )
         Text(
             text = "Terms and Conditions",
             style = MaterialTheme.typography.labelSmall.copy(fontSize = 18.sp),
@@ -76,16 +54,23 @@ fun TermsAndConditions(
             modifier = Modifier
                 .padding(top = 0.dp, start = 0.dp, end = 0.dp, bottom = 0.dp)
         )
-        TextButton(onClick = { onClickRegister() }, content = {
-            Text(
-                text = "Register",
-                style = MaterialTheme.typography.labelSmall.copy(fontSize = 14.sp),
-                maxLines = 1,
-                overflow = TextOverflow.Ellipsis,
-                modifier = Modifier
-                    .padding(top = 0.dp, start = 0.dp, end = 0.dp, bottom = 0.dp)
-            )
-        })
+        TextButton(
+            onClick = {
+                registrationViewModel.acceptTCs()
+                registrationViewModel.registerUser()
+                (context as RegistrationActivity).onTermsAndConditionsAccepted()
+            },
+            content = {
+                Text(
+                    text = "Register",
+                    style = MaterialTheme.typography.labelSmall.copy(fontSize = 14.sp),
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis,
+                    modifier = Modifier
+                        .padding(top = 0.dp, start = 0.dp, end = 0.dp, bottom = 0.dp),
+                )
+            },
+        )
     }
 }
 
@@ -96,8 +81,7 @@ fun TermsAndConditionsPreview() {
         TermsAndConditions(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(all = 0.dp),
-            onClickRegister = {}
+                .padding(all = 0.dp)
         )
     }
 }
