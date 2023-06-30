ab546fe8e05f7729b81dd9a1ba548fa2100294fb
VSemenyshyn <Viktoriia.Palenka@gmail.com>
2023-06-30 16:40:25 +0300

    Compose - before making email verification.

--- app/src/main/java/com/simenko/qmapp/ui/main/settings/SettingsFragment.kt
+++ app/src/main/java/com/simenko/qmapp/ui/main/settings/SettingsFragment.kt
@@ -8,26 +8,37 @@ import android.view.ViewGroup
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.fragment.app.Fragment
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.simenko.qmapp.ui.user.login.LoginActivity
 import com.simenko.qmapp.ui.theme.QMAppTheme
+import com.simenko.qmapp.ui.user.repository.UserErrorState
+import com.simenko.qmapp.ui.user.repository.UserLoggedInState
 import dagger.hilt.android.AndroidEntryPoint
+import kotlin.random.Random
 
 @AndroidEntryPoint
 class SettingsFragment : Fragment() {
@@ -69,6 +80,18 @@ fun Settings(
     onClickLogOut: () -> Unit,
 ) {
     val settingsViewModel: SettingsViewModel = hiltViewModel()
+    val userState by settingsViewModel.userState.collectAsStateWithLifecycle()
+
+    var error by rememberSaveable { mutableStateOf("") }
+    var msg by rememberSaveable { mutableStateOf("") }
+
+    userState.getContentIfNotHandled()?.let {
+        if (it is UserErrorState) {
+            error = it.error ?: "unknown error"
+        } else if (it is UserLoggedInState) {
+            msg = it.msg
+        }
+    }
 
     Column(
         verticalArrangement = Arrangement.Center,
@@ -76,12 +99,25 @@ fun Settings(
         modifier = Modifier
             .padding(all = 0.dp)
     ) {
+        if (error != "")
+            Text(
+                text = error,
+                style = MaterialTheme.typography.labelSmall.copy(fontSize = 14.sp, color = MaterialTheme.colorScheme.error),
+                modifier = Modifier
+                    .padding(all = 5.dp),
+                textAlign = TextAlign.Center
+            )
+        Spacer(modifier = Modifier.height(10.dp))
         TextButton(
             modifier = Modifier.width(150.dp),
-            onClick = { settingsViewModel.refreshNotifications() },
+            onClick = {
+                error = ""
+                msg = ""
+                settingsViewModel.setUserJobRole("Quality Manager - " + (1..300).random())
+            },
             content = {
                 Text(
-                    text = "Clear notifications",
+                    text = "Add request",
                     style = MaterialTheme.typography.labelSmall.copy(fontSize = 14.sp),
                     maxLines = 1,
                     overflow = TextOverflow.Ellipsis,
@@ -111,6 +147,15 @@ fun Settings(
             border = BorderStroke(1.dp, MaterialTheme.colorScheme.primary),
             shape = MaterialTheme.shapes.medium
         )
+        Spacer(modifier = Modifier.height(10.dp))
+        if (msg != "")
+            Text(
+                text = msg,
+                style = MaterialTheme.typography.labelSmall.copy(fontSize = 14.sp),
+                modifier = Modifier
+                    .padding(all = 5.dp),
+                textAlign = TextAlign.Center
+            )
     }
 }
 
