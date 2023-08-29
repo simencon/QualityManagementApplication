21d78363be6350256f21645f4b28371f80be2647
VSemenyshyn <Viktoriia.Palenka@gmail.com>
2023-08-29 23:30:57 +0300

    Good for now - state on 20230829

--- app/src/main/java/com/simenko/qmapp/ui/neworder/steps/__________OrderPlaserComposition.kt
+++ app/src/main/java/com/simenko/qmapp/ui/neworder/steps/__________OrderPlaserComposition.kt
@@ -3,25 +3,20 @@ package com.simenko.qmapp.ui.neworder.steps
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.grid.rememberLazyGridState
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.simenko.qmapp.domain.entities.DomainTeamMember
 import com.simenko.qmapp.ui.dialogs.scrollToSelectedItem
+import com.simenko.qmapp.ui.neworder.ItemToSelect
 import com.simenko.qmapp.ui.neworder.NewItemViewModel
-import com.simenko.qmapp.ui.theme.Primary
-import com.simenko.qmapp.ui.theme.TertiaryContainer
 
 @Composable
 fun PlacersSelection(
@@ -42,21 +37,20 @@ fun PlacersSelection(
         )
     }
 
-    items.let {
-        LazyHorizontalGrid(
-            rows = GridCells.Fixed(1),
-            state = gritState,
-            contentPadding = PaddingValues(horizontal = 16.dp),
-            horizontalArrangement = Arrangement.spacedBy(8.dp),
-            verticalArrangement = Arrangement.spacedBy(8.dp),
-            modifier = modifier.height(60.dp)
-        ) {
-            items(it.size) { item ->
-                InvestigationPlacerCard(
-                    inputForOrder = it[item],
-                    onClick = { onSelectLambda(it) }
-                )
-            }
+
+    LazyHorizontalGrid(
+        rows = GridCells.Fixed(1),
+        state = gritState,
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        modifier = modifier.height(60.dp)
+    ) {
+        items(items = items, key = { it.id }) { item ->
+            InvestigationPlacerCard(
+                inputForOrder = item,
+                onClick = { onSelectLambda(it) }
+            )
         }
     }
 }
@@ -66,23 +60,5 @@ fun InvestigationPlacerCard(
     inputForOrder: DomainTeamMember,
     onClick: (Int) -> Unit
 ) {
-
-    val btnBackgroundColor = if (inputForOrder.isSelected) Primary else TertiaryContainer
-    val btnContentColor = if (inputForOrder.isSelected) Color.White else Color.Black
-    val btnColors = ButtonDefaults.buttonColors(
-        contentColor = btnContentColor,
-        containerColor = btnBackgroundColor
-    )
-
-    Row(
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        TextButton(
-            colors = btnColors,
-            modifier = Modifier
-                .width(224.dp)
-                .height(56.dp),
-            onClick = { onClick(inputForOrder.id) }
-        ) { Text(text = inputForOrder.fullName) }
-    }
+    ItemToSelect(Triple(inputForOrder.id, inputForOrder.fullName, inputForOrder.isSelected), onClick)
 }
