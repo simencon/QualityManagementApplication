b8087ad7d32dfc3b01133a7dff5b6b82ca3bfcbe
VSemenyshyn <Viktoriia.Palenka@gmail.com>
2023-09-03 20:12:34 +0300

    Stage before adding possibility to edit user own data!

--- app/src/main/java/com/simenko/qmapp/ui/user/registration/RegistrationViewModel.kt
+++ app/src/main/java/com/simenko/qmapp/ui/user/registration/RegistrationViewModel.kt
@@ -33,13 +33,8 @@ class RegistrationViewModel @Inject constructor(
         _principleToRegister = principle
     }
 
-    fun acceptTCs() {
-        _acceptedTCs = true
-    }
-
     fun registerUser() {
         assert(_principleToRegister != null)
-        assert(_acceptedTCs == true)
         _userViewModel.updateLoadingState(Pair(true, null))
         userRepository.registerUser(_principleToRegister!!)
     }
