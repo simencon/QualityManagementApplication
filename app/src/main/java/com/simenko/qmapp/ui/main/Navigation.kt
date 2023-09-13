9d70e1f9be54356dd5862921c340440bc2be4ba3
VSemenyshyn <Viktoriia.Palenka@gmail.com>
2023-09-13 15:22:22 +0300

    handled to read args when transaction completely finished - improved (implemented only for employees).

--- app/src/main/java/com/simenko/qmapp/ui/main/Navigation.kt
+++ app/src/main/java/com/simenko/qmapp/ui/main/Navigation.kt
@@ -11,12 +11,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -83,6 +78,7 @@ fun Navigation(
 
                 QMAppTheme {
                     EmployeeComposition(onClickEdit = { id ->
+                        teamModel.setSelectedRecord(NoRecord.num)
                         teamModel.setAddEditMode(AddEditMode.EDIT_EMPLOYEE)
                         navController.navigate(Screen.Main.Team.EmployeeAddEdit.withArgs(id.toString()))
                     })
