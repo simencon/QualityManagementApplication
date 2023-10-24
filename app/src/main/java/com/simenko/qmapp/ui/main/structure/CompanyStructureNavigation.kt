81090faff2d6a3ea7f750312c218a786c10115dd
RF4786 <romansemenyshyn@gmail.com>
2023-10-24 18:25:37 +0300

    Added Form for Line - adjusting stage

--- app/src/main/java/com/simenko/qmapp/ui/main/structure/CompanyStructureNavigation.kt
+++ app/src/main/java/com/simenko/qmapp/ui/main/structure/CompanyStructureNavigation.kt
@@ -3,6 +3,8 @@ package com.simenko.qmapp.ui.main.structure
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavGraphBuilder
+import com.simenko.qmapp.ui.main.structure.forms.line.LineForm
+import com.simenko.qmapp.ui.main.structure.forms.line.LineViewModel
 import com.simenko.qmapp.ui.main.structure.forms.operation.OperationForm
 import com.simenko.qmapp.ui.main.structure.forms.operation.OperationViewModel
 import com.simenko.qmapp.ui.main.structure.steps.CompanyStructure
@@ -19,6 +21,10 @@ fun NavGraphBuilder.companyStructureNavigation(mainScreenPadding: PaddingValues)
         composable(destination = Route.Main.CompanyStructure.OperationAddEdit) {
             val viewModel: OperationViewModel = hiltViewModel()
             OperationForm(viewModel = viewModel)
+        }
+        composable(destination = Route.Main.CompanyStructure.LineAddEdit) {
+            val viewModel: LineViewModel = hiltViewModel()
+            LineForm(viewModel = viewModel)
         }
     }
 }
