66e87c562dbcb993d0defa3593ce544a3eb197ca
simenko <romansemenyshyn@gmail.com>
2024-04-16 19:17:54 +0300

    Added AuthorizationInterceptor and draft of ErrorHandlerInterceptor.

--- app/src/main/java/com/simenko/qmapp/BaseApplication.kt
+++ app/src/main/java/com/simenko/qmapp/BaseApplication.kt
@@ -7,6 +7,7 @@ import androidx.hilt.work.HiltWorkerFactory
 import androidx.work.*
 import com.simenko.qmapp.other.Constants.SYNC_NOTIFICATION_CHANNEL_ID
 import com.simenko.qmapp.other.Constants.SYNC_NOTIFICATION_CHANNEL_NAME
+import com.simenko.qmapp.retrofit.implementation.interceptors.ActivityLifeCycleCallbackImpl
 import com.simenko.qmapp.works.SyncEntitiesWorker
 import com.simenko.qmapp.works.SyncPeriods
 import com.simenko.qmapp.works.WorkerKeys.EXCLUDE_MILLIS
@@ -17,6 +18,7 @@ import javax.inject.Inject
 
 @HiltAndroidApp
 class BaseApplication : Application(), Configuration.Provider {
+
     @Inject
     lateinit var workerFactory: HiltWorkerFactory
     override val workManagerConfiguration: Configuration
@@ -24,8 +26,11 @@ class BaseApplication : Application(), Configuration.Provider {
             .setWorkerFactory(workerFactory)
             .build()
 
+    private val activityLifecycleCallback = ActivityLifeCycleCallbackImpl()
+
     override fun onCreate() {
         super.onCreate()
+        registerActivityLifecycleCallbacks(activityLifecycleCallback)
         val notificationChannel = NotificationChannel(
             SYNC_NOTIFICATION_CHANNEL_ID,
             SYNC_NOTIFICATION_CHANNEL_NAME,
@@ -34,6 +39,8 @@ class BaseApplication : Application(), Configuration.Provider {
         val notificationManager = getSystemService(NotificationManager::class.java)
         notificationManager.createNotificationChannel(notificationChannel)
     }
+
+    fun getCurrentActivity() = activityLifecycleCallback.getCurrentActivity()
 
     fun setupOneTimeSync() {
         scheduleOneTimeSyncWork(SyncPeriods.LAST_HOUR, Duration.ofSeconds(0L))
