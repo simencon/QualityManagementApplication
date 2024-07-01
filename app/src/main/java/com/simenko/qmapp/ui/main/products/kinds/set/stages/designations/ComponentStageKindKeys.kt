d50a77294e2ffdee641f2afc29e3b4241746b0f8
simenko <romansemenyshyn@gmail.com>
2024-07-01 20:54:48 +0300

    crude done for ComponentStageKindKeys

--- app/src/main/java/com/simenko/qmapp/ui/main/products/kinds/set/stages/designations/ComponentStageKindKeys.kt
+++ app/src/main/java/com/simenko/qmapp/ui/main/products/kinds/set/stages/designations/ComponentStageKindKeys.kt
@@ -1,8 +1,10 @@
 package com.simenko.qmapp.ui.main.products.kinds.set.stages.designations
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -27,6 +29,7 @@ import com.simenko.qmapp.domain.SelectedNumber
 import com.simenko.qmapp.domain.entities.products.DomainComponentStageKind
 import com.simenko.qmapp.other.Constants.DEFAULT_SPACE
 import com.simenko.qmapp.ui.common.InfoLine
+import com.simenko.qmapp.ui.common.SingleChoiceDialog
 import com.simenko.qmapp.ui.main.products.designations.KeyCard
 import com.simenko.qmapp.ui.navigation.Route
 
@@ -39,6 +42,10 @@ fun ComponentStageKindKeys(
     val componentStageKind by viewModel.componentStageKind.collectAsStateWithLifecycle(DomainComponentStageKind.DomainComponentStageKindComplete())
     val items by viewModel.componentStageKindKeys.collectAsStateWithLifecycle(listOf())
 
+    val availableItems by viewModel.availableKeys.collectAsStateWithLifecycle(listOf())
+    val isAddItemDialogVisible by viewModel.isAddItemDialogVisible.collectAsStateWithLifecycle()
+    val searchString by viewModel.itemToAddSearchStr.collectAsStateWithLifecycle()
+
     val onClickActionsLambda = remember<(ID) -> Unit> { { viewModel.setComponentStageKindKeysVisibility(aId = SelectedNumber(it)) } }
     val onClickDeleteLambda = remember<(ID) -> Unit> { { viewModel.onDeleteComponentStageKindKeyClick(it) } }
 
@@ -46,25 +53,41 @@ fun ComponentStageKindKeys(
 
     val listState = rememberLazyListState()
 
-    Column(horizontalAlignment = Alignment.Start, verticalArrangement = Arrangement.Bottom) {
-        Spacer(modifier = Modifier.height(10.dp))
-        InfoLine(
-            modifier = modifier.padding(start = DEFAULT_SPACE.dp),
-            title = "Product line",
-            body = componentStageKind.componentKind.productKind.productLine.manufacturingProject.projectSubject ?: NoString.str
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(all = 0.dp)
+    ) {
+        if (isAddItemDialogVisible) SingleChoiceDialog(
+            items = availableItems,
+            addIsEnabled = availableItems.any { it.isSelected },
+            onDismiss = { viewModel.setAddItemDialogVisibility(false) },
+            searchString = searchString,
+            onSearch = viewModel::setItemToAddSearchStr,
+            onItemSelect = { viewModel.onItemSelect(it) },
+            onAddClick = { viewModel.onAddItemClick() }
         )
-        InfoLine(modifier = modifier.padding(start = DEFAULT_SPACE.dp), title = "Product", body = componentStageKind.componentKind.productKind.productKind.productKindDesignation)
-        InfoLine(modifier = modifier.padding(start = DEFAULT_SPACE.dp), title = "Component", body = componentStageKind.componentKind.componentKind.componentKindDescription)
-        InfoLine(modifier = modifier.padding(start = DEFAULT_SPACE.dp), title = "Component stage", body = componentStageKind.componentStageKind.componentStageDescription)
-        HorizontalDivider(modifier = Modifier.height(1.dp), color = MaterialTheme.colorScheme.secondary)
-        LazyColumn(modifier = modifier, state = listState, horizontalAlignment = Alignment.End, verticalArrangement = Arrangement.Center) {
-            items(items = items, key = { it.componentStageKindKey.id }) { key ->
-                KeyCard(
-                    key = key.key,
-                    onClickActions = { onClickActionsLambda(it) },
-                    onClickDelete = { onClickDeleteLambda(it) },
-                    actionButtonsImages = arrayOf(Icons.Filled.Delete),
-                )
+
+        Column(horizontalAlignment = Alignment.Start, verticalArrangement = Arrangement.Bottom) {
+            Spacer(modifier = Modifier.height(10.dp))
+            InfoLine(
+                modifier = modifier.padding(start = DEFAULT_SPACE.dp),
+                title = "Product line",
+                body = componentStageKind.componentKind.productKind.productLine.manufacturingProject.projectSubject ?: NoString.str
+            )
+            InfoLine(modifier = modifier.padding(start = DEFAULT_SPACE.dp), title = "Product", body = componentStageKind.componentKind.productKind.productKind.productKindDesignation)
+            InfoLine(modifier = modifier.padding(start = DEFAULT_SPACE.dp), title = "Component", body = componentStageKind.componentKind.componentKind.componentKindDescription)
+            InfoLine(modifier = modifier.padding(start = DEFAULT_SPACE.dp), title = "Component stage", body = componentStageKind.componentStageKind.componentStageDescription)
+            HorizontalDivider(modifier = Modifier.height(1.dp), color = MaterialTheme.colorScheme.secondary)
+            LazyColumn(modifier = modifier, state = listState, horizontalAlignment = Alignment.End, verticalArrangement = Arrangement.Center) {
+                items(items = items, key = { it.componentStageKindKey.id }) { key ->
+                    KeyCard(
+                        key = key.key,
+                        onClickActions = { onClickActionsLambda(it) },
+                        onClickDelete = { onClickDeleteLambda(it) },
+                        actionButtonsImages = arrayOf(Icons.Filled.Delete),
+                    )
+                }
             }
         }
     }
--- app/src/main/java/com/simenko/qmapp/ui/main/products/kinds/set/stages/designations/ComponentStageKindKeysViewModel.kt
+++ app/src/main/java/com/simenko/qmapp/ui/main/products/kinds/set/stages/designations/ComponentStageKindKeysViewModel.kt
@@ -2,9 +2,13 @@ package com.simenko.qmapp.ui.main.products.kinds.set.stages.designations
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.simenko.qmapp.domain.EmptyString
 import com.simenko.qmapp.domain.ID
 import com.simenko.qmapp.domain.NoRecord
 import com.simenko.qmapp.domain.SelectedNumber
+import com.simenko.qmapp.domain.entities.products.DomainComponentStageKind
+import com.simenko.qmapp.domain.entities.products.DomainComponentStageKindKey
+import com.simenko.qmapp.other.Status
 import com.simenko.qmapp.repository.ProductsRepository
 import com.simenko.qmapp.storage.Storage
 import com.simenko.qmapp.ui.main.main.MainPageHandler
@@ -16,10 +20,14 @@ import com.simenko.qmapp.utils.InvestigationsUtils.setVisibility
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.channels.consumeEach
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -31,9 +39,13 @@ class ComponentStageKindKeysViewModel @Inject constructor(
     private val repository: ProductsRepository,
     val storage: Storage,
 ) : ViewModel() {
-    private val _componentStageKindId = MutableStateFlow(NoRecord.num)
+    private val _componentStageKind = MutableStateFlow(DomainComponentStageKind.DomainComponentStageKindComplete())
     private val _componentStageKindKeysVisibility = MutableStateFlow(Pair(SelectedNumber(NoRecord.num), NoRecord))
-    private val _componentStageKindKeys = _componentStageKindId.flatMapLatest { repository.componentStageKindKeys(it) }
+    private val _componentStageKindKeys = _componentStageKind.flatMapLatest { repository.componentStageKindKeys(it.componentStageKind.id) }
+
+    private val _isAddItemDialogVisible = MutableStateFlow(false)
+    private val _itemToAddId: MutableStateFlow<ID> = MutableStateFlow(NoRecord.num)
+    private val _itemToAddSearchStr: MutableStateFlow<String> = MutableStateFlow(EmptyString.str)
 
     /**
      * Main page setup -------------------------------------------------------------------------------------------------------------------------------
@@ -41,15 +53,15 @@ class ComponentStageKindKeysViewModel @Inject constructor(
     private var mainPageHandler: MainPageHandler? = null
 
     fun onEntered(route: Route.Main.ProductLines.ProductKinds.ProductSpecification.ComponentStageKindKeys.ComponentStageKindKeysList) {
-        viewModelScope.launch {
+        viewModelScope.launch(Dispatchers.IO) {
             if (mainPageHandler == null) {
-                _componentStageKindId.value = route.componentStageKindId
+                _componentStageKind.value = repository.componentStageKind(route.componentStageKindId)
                 _componentStageKindKeysVisibility.value = Pair(SelectedNumber(route.componentStageKindKeyId), NoRecord)
             }
 
             mainPageHandler = MainPageHandler.Builder(Page.COMPONENT_STAGE_KIND_KEYS, mainPageState)
                 .setOnNavMenuClickAction { appNavigator.navigateBack() }
-                .setOnFabClickAction { onAddComponentStageKindKeyClick(route.componentStageKindId) }
+                .setOnFabClickAction { onAddComponentStageKindKeyClick() }
                 .setOnPullRefreshAction { updateCompanyProductsData() }
                 .build()
                 .apply { setupMainPage(0, true) }
@@ -66,7 +78,7 @@ class ComponentStageKindKeysViewModel @Inject constructor(
     /**
      * UI state --------------------------------------------------------------------------------------------------------------------------------------
      * */
-    val componentStageKind get() = _componentStageKindId.flatMapLatest { flow { emit(repository.componentStageKind(it)) } }.flowOn(Dispatchers.IO)
+    val componentStageKind = _componentStageKind.asStateFlow()
 
     val componentStageKindKeys = _componentStageKindKeys.flatMapLatest { productKindKeys ->
         _componentStageKindKeysVisibility.flatMapLatest { visibility ->
@@ -75,23 +87,115 @@ class ComponentStageKindKeysViewModel @Inject constructor(
             }
             flow { emit(cpy) }
         }
+    }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(), emptyList())
+
+    val availableKeys = _componentStageKind.flatMapLatest { componentStageKind ->
+        _componentStageKindKeys.flatMapLatest { addedKeys ->
+            _itemToAddSearchStr.flatMapLatest { searchStr ->
+                _itemToAddId.flatMapLatest { id ->
+                    repository.productLineKeys(componentStageKind.componentKind.productKind.productLine.manufacturingProject.id).map { list ->
+                        list
+                            .subtract(addedKeys.map { it.key }.toSet())
+                            .filter {
+                                if (searchStr.isNotEmpty()) {
+                                    it.productLineKey.componentKeyDescription?.lowercase()?.contains(searchStr.lowercase()) ?: false
+                                            || it.productLineKey.componentKey.lowercase().contains(searchStr.lowercase())
+                                } else {
+                                    true
+                                }
+                            }
+                            .map { item -> item.copy(isSelected = item.productLineKey.id == id) }
+                    }
+                }
+            }
+        }
+    }
+
+    val isAddItemDialogVisible = _isAddItemDialogVisible.asStateFlow()
+    fun setAddItemDialogVisibility(value: Boolean) {
+        if (!value) {
+            _itemToAddSearchStr.value = EmptyString.str
+            _itemToAddId.value = NoRecord.num
+        }
+        _isAddItemDialogVisible.value = value
+    }
+
+    val itemToAddSearchStr = _itemToAddSearchStr.asStateFlow()
+    fun setItemToAddSearchStr(value: String) {
+        if (_itemToAddSearchStr.value != value) _itemToAddSearchStr.value = value
     }
 
     /**
      * REST operations -------------------------------------------------------------------------------------------------------------------------------
      * */
-    fun onDeleteComponentStageKindKeyClick(it: ID) {
-        TODO("Not yet implemented")
+    fun onDeleteComponentStageKindKeyClick(it: ID) = viewModelScope.launch(Dispatchers.IO) {
+        componentStageKindKeys.value.find { it.key.isExpanded }?.let { itemToDelete ->
+            if (itemToDelete.componentStageKindKey.keyId == it)
+                with(repository) {
+                    deleteComponentStageKindKey(itemToDelete.componentStageKindKey.id).consumeEach { event ->
+                        event.getContentIfNotHandled()?.let { resource ->
+                            when (resource.status) {
+                                Status.LOADING -> mainPageHandler?.updateLoadingState?.invoke(Pair(true, null))
+                                Status.SUCCESS -> mainPageHandler?.updateLoadingState?.invoke(Pair(false, null))
+                                Status.ERROR -> mainPageHandler?.updateLoadingState?.invoke(Pair(false, resource.message))
+                            }
+                        }
+                    }
+                }
+        }
     }
 
-    private fun updateCompanyProductsData() {
-        TODO("Not yet implemented")
+    private fun updateCompanyProductsData() = viewModelScope.launch(Dispatchers.IO) {
+        try {
+            mainPageHandler?.updateLoadingState?.invoke(Pair(true, null))
+
+            repository.syncComponentStageKindsKeys()
+
+            mainPageHandler?.updateLoadingState?.invoke(Pair(false, null))
+        } catch (e: Exception) {
+            mainPageHandler?.updateLoadingState?.invoke(Pair(false, e.message))
+        }
     }
 
     /**
      * Navigation ------------------------------------------------------------------------------------------------------------------------------------
      * */
-    private fun onAddComponentStageKindKeyClick(it: ID) {
-        TODO("Not yet implemented")
+    private fun onAddComponentStageKindKeyClick() {
+        _isAddItemDialogVisible.value = true
+    }
+
+    fun onItemSelect(id: ID) {
+        _itemToAddId.value = id
+    }
+
+    fun onAddItemClick() = viewModelScope.launch(Dispatchers.IO) {
+        val componentStageKindId = _componentStageKind.value.componentStageKind.id
+        val keyId = _itemToAddId.value
+        if (componentStageKindId != NoRecord.num && keyId != NoRecord.num) {
+            //  make record!
+            with(repository) {
+                _isAddItemDialogVisible.value = false
+                _itemToAddId.value = NoRecord.num
+                insertComponentStageKindKey(
+                    DomainComponentStageKindKey(
+                        id = NoRecord.num,
+                        componentStageKindId = componentStageKindId,
+                        keyId = keyId
+                    )
+                ).consumeEach { event ->
+                    event.getContentIfNotHandled()?.let { resource ->
+                        when (resource.status) {
+                            Status.LOADING -> mainPageHandler?.updateLoadingState?.invoke(Pair(true, null))
+                            Status.SUCCESS -> {
+                                mainPageHandler?.updateLoadingState?.invoke(Pair(false, null))
+                                resource.data?.let { setComponentStageKindKeysVisibility(dId = SelectedNumber(it.keyId)) }
+                            }
+
+                            Status.ERROR -> mainPageHandler?.updateLoadingState?.invoke(Pair(true, resource.message))
+                        }
+                    }
+                }
+            }
+        }
     }
 }
