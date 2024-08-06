e6ba6c3ed7c713f7898071a43dacb67d9a07a373
simenko <romansemenyshyn@gmail.com>
2024-08-06 19:22:06 +0300

    stage before migration to kotlin serialize

--- app/src/main/java/com/simenko/qmapp/room/implementation/dao/products/specification/ComponentDao.kt
+++ app/src/main/java/com/simenko/qmapp/room/implementation/dao/products/specification/ComponentDao.kt
@@ -2,6 +2,7 @@ package com.simenko.qmapp.room.implementation.dao.products.specification
 
 import androidx.room.Dao
 import androidx.room.Query
+import androidx.room.Transaction
 import com.simenko.qmapp.domain.ID
 import com.simenko.qmapp.room.contract.DaoBaseModel
 import com.simenko.qmapp.room.entities.products.DatabaseComponent
@@ -18,6 +19,7 @@ abstract class ComponentDao: DaoBaseModel<ID, ID, DatabaseComponent> {
     @Query("select * from `4_components` where keyId = :parentId order by id  asc")
     abstract fun getRecordsByParentId(parentId: ID): List<DatabaseComponent>
 
+    @Transaction
     @Query("SELECT * FROM `components_complete` WHERE id = :id")
     abstract fun getRecordById(id: ID): DatabaseComponent.DatabaseComponentComplete?
 }
